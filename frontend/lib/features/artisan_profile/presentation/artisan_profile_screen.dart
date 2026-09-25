4ccd15cf1c472bce8bc3d43ba55cba35017f63e2
komal-kasaudhan <komalkasaudhan@gmail.com>
2026-09-26 05:20:06 +0530

    error fixing 3

--- frontend/lib/features/artisan_profile/presentation/artisan_profile_screen.dart
+++ frontend/lib/features/artisan_profile/presentation/artisan_profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import '../../../core/constants/app_constants.dart';
 import '../../../core/theme/app_theme.dart';
 import '../../../core/localization/app_localizations.dart';
 import '../../../core/widgets/clay_widgets.dart';
